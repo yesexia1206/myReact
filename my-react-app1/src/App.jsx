bf9cc476e255cc35eac3e86b21b0978fb2fda200
yesexia <yaya871206mike@gmail.com>
2024-11-12 16:56:54 +0800

    upda-2024-11-12-1

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,63 +1,66 @@
+import "./style-text-1112.css"
 
 function App() {
 
-  //建立物件變數
-  const person = {
-
-    s1: {
-      name: "同學1",
-      age: 10,
-    },
-    s2: {
-      name: "同學2",
-      age: 11,
-    },
-    s3: {
-      name: "同學3",
-      age: 12,
-    },
-  }
-
-  //多筆資料的物件解構寫法方法1:一般寫法
-  // const { s1, s2, s3 } = person;
-
-  //多筆資料的物件解構寫法方法2:解構+展開...(其餘運算子)
-  //解構s1，展開s2,s3
-  const { s1, ...other } = person;
-  // console.log(s1);
-  // console.log(other.s3);
-
-  const person2={
-    name:"同學1",
-    age:"10",
-  }
-
-  //一般寫法
-  // function showName(obj){
-  //   console.log(obj);
-  //   console.log(obj.name);
-  //   console.log(obj.age);
-  // }
-  //將物件屬性直接解構給函式參數
-  function showName({name,age}){
-    console.log(name);
-    console.log(age);
-  }
-  showName(person2);
-
   return (
     <>
-    <div>{`同學1姓名:${s1.name}`}</div>
-    <div>{`同學3姓名:${other.s3.name}`}</div>
-      {/* <div>第一位同學姓名:{s1.name}</div>
-      <div>第一位同學年紀:{s1.age}</div>
-      <hr />
-      <div>第二位同學姓名:{s2.name}</div>
-      <div>第二位同學年紀:{s2.age}</div>
-      <hr />
-      <div>第三位同學姓名:{s3.name}</div>
-      <div>第三位同學年紀:{s3.age}</div>
-      <hr /> */}
+      
+        <div className="ad">
+          <img src="images/banner.jpg" alt="頁首廣告" />
+        </div>
+        <section className="feature">
+          <div>
+            <h2>教學影音</h2>
+            <p>教學影音教學影音教學影音教學影音教學影音教學影音教學影音</p>
+            </div>
+          <div>
+            <h2>良性互動</h2>
+            <p>良性互動良性互動良性互動良性互動良性互動良性互動良性互動</p>
+            </div>
+          <div>
+            <h2>趨勢分享</h2>
+            <p>趨勢分享趨勢分享趨勢分享趨勢分享趨勢分享趨勢分享趨勢分享</p>
+            </div>
+        </section>
+        <div className="solgan">
+          <h2>讓學習成為一種習慣</h2>
+        </div>
+        <section className="curriculum">
+            <h2>推薦課程</h2>
+          <div className="Class">
+            <div className="list">
+              <img src="images/unity.jpg" alt="unity5課程" />
+              <h2>Unity5</h2>
+              <p className="p-blue">Unity5Unity5Unity5</p>
+              <p>講師:Unity</p>
+              <p>影音課程時數:4小時</p>
+              <p><span className="span1">原價NT$1600</span><span className="span2">NT$1200</span></p>
+              <button>付款上課去</button>
+            </div>
+
+            <div className="list">
+              <img src="images/gamesalad.jpg" alt="gamesalad課程" />
+              <h2>Gamesalad 2D 遊戲製作</h2>
+              <p className="p-blue">gamesaleadgamesalead</p>
+              <p>講師:gamesalead</p>
+              <p>影音課程時數:8小時</p>
+              <p><span className="span1">原價NT$3200</span><span className="span2">NT$1600</span></p>
+              <button>付款上課去</button>
+            </div>
+
+            <div className="list">
+              <img src="images/gwd.jpg" alt="gwd課程" />
+              <h2>Google Web Design</h2>
+              <p className="p-blue">gwdgwdgwd</p>
+              <p>講師:google</p>
+              <p>影音課程時數:8小時</p>
+              <p><span className="span1">原價NT$3200</span><span className="span2">NT$1600</span></p>
+              <button>付款上課去</button>
+            </div>
+          </div>
+        </section>
+      
+      <footer><h2>劉哲宇</h2></footer>
     </>
   )
 }
