e4e5f7bcaeead91f9a9485239c2d3f7df9495828
yesexia <yaya871206mike@gmail.com>
2024-12-03 09:37:27 +0800

    upda-2024-12-03

--- my-react-app1/vite.config.js
+++ my-react-app1/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //建立一個開發中的路徑
+  //process.env.NODE_ENV => 取得node環境中的變數
+  //如果目前的路徑等於產品名稱，就執行倉庫的路徑
+  base: process.env.NODE_ENV === 'production' ? '/my-react-app1/' : '/',
   plugins: [react()],
 })
